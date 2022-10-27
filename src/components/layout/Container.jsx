29c11caafa5a0e1f9f75074e9c904594f064e5e1
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-10-26 23:08:11 -0300

    Criado componente de post

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components'
-
 const StyledContainer = styled.div`
   display: flex;
   justify-content: center;
+  @media (max-width: 700px) {
+    padding: 10px;
+  }
 `
 
 const StyledChildren = styled.div`
   max-width: 700px;
   width: 100%;
 `
-
 function Container ({ children }) {
   return (
     <StyledContainer>
@@ -19,5 +20,4 @@ function Container ({ children }) {
     </StyledContainer>
   )
 }
-
 export default Container
