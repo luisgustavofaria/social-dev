0403f1844153ae7f08011d4e0a10bc4dc3cd3870
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-10-30 13:44:18 -0300

    Criado servico de autenticacao

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,7 +1,12 @@
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
+
 import createHandler from '../../../lib/middlewares/nextConnect'
 import validate from '../../../lib/middlewares/validation'
 import { signupUser } from '../../../modules/user/user.service'
+
+import { ironConfig } from '../../../lib/middlewares/ironSession'
+
 const postSchema = Joi.object({
   firstName: Joi.string().required().max(50),
   lastName: Joi.string().required().max(50),
@@ -9,17 +14,22 @@ const postSchema = Joi.object({
   email: Joi.string().email().required().max(100),
   password: Joi.string().required().max(50).min(6),
 })
-
 const signup = createHandler()
-
 signup.post(validate({ body: postSchema }), async (req, res) => {
   try {
     const user = await signupUser(req.body)
-    res.status(201).json(user)
+
+    req.session.user = {
+      id: user._id,
+      user: user.user
+    }
+    await req.session.save()
+
+    res.status(201).json({ ok: true })
   } catch (err) {
     console.error(err)
     throw err
   }
 })
 
-export default signup
+export default withIronSessionApiRoute(signup, ironConfig)
