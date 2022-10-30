2b2ab67460c8d1b9c990e5a2232460ed592828ac
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-10-30 15:28:13 -0300

    Criado useState para envio de formulario

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,35 +1,61 @@
-import Joi from 'joi'
-import { withIronSessionApiRoute } from 'iron-session/next'
+import { useState } from 'react'
+import styled from 'styled-components'
+import Link from 'next/link'
 
-import createHandler from '../../../lib/middlewares/nextConnect'
-import validate from '../../../lib/middlewares/validation'
-import { signupUser } from '../../../modules/user/user.service'
+import ImageWithSpace from '../../../src/components/layout/ImageWithSpace'
+import H1 from '../../../src/components/typography/H1'
+import H2 from '../../../src/components/typography/H2'
+import H4 from '../../../src/components/typography/H4'
+import Button from '../../../src/components/inputs/Button'
+import Input from '../../../src/components/inputs/Input'
+const FormContainer = styled.div`
+  margin-top: 60px;
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+  gap: 20px;
+`
+const Text = styled.p`
+  text-align: center;
+`
 
-import { ironConfig } from '../../../lib/middlewares/ironSession'
+function SignupPage () {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [user, setUser] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-const postSchema = Joi.object({
-  firstName: Joi.string().required().max(50),
-  lastName: Joi.string().required().max(50),
-  user: Joi.string().required().max(30),
-  email: Joi.string().email().required().max(100),
-  password: Joi.string().required().max(50).min(6),
-})
-const signup = createHandler()
-signup.post(validate({ body: postSchema }), async (req, res) => {
-  try {
-    const user = await signupUser(req.body)
-
-    req.session.user = {
-      id: user._id,
-      user: user.user
-    }
-    await req.session.save()
-
-    res.status(201).json({ ok: true })
-  } catch (err) {
-    console.error(err)
-    throw err
+  const handleForm = (event) => {
+    event.preventDefault()
+    console.log({
+      firstName,
+      lastName,
+      user,
+      email,
+      password
+    })
   }
-})
 
-export default withIronSessionApiRoute(signup, ironConfig)
+  return (
+    <ImageWithSpace>
+      <H1># Social Dev</H1>
+      <H4>Tudo que acontece no mundo dev, está aqui!</H4>
+      <FormContainer>
+        <H2>Crie sua conta</H2>
+        <Form onSubmit={handleForm}>
+          <Input label="Nome" onChange={({ target }) => {setFirstName(target.value)}} />
+          <Input label="Sobrenome" onChange={({ target }) => setLastName(target.value)} />
+          <Input label="Usuário" onChange={({ target }) => setUser(target.value)} />
+          <Input label="Email" type="email" onChange={({ target }) => setEmail(target.value)} />
+          <Input label="Senha" type="password" onChange={({ target }) => setPassword(target.value)} />
+          <Button>Cadastrar</Button>
+        </Form>
+        <Text>Já possui uma conta? <Link href="/login">Faça seu login</Link></Text>
+      </FormContainer>
+    </ImageWithSpace>
+  )
+}
+export default SignupPage
