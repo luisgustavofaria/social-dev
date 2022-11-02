939211bcdebdf502fd6b11c4fae78b76882f6112
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-11-02 20:45:20 -0300

    Criado busca de posts usando axios

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,26 +1,39 @@
+import { useEffect } from 'react'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 import theme from '../src/theme'
+
 const GlobalStyle = createGlobalStyle`
   * {
     padding: 0;
     margin: 0;
   }
+
   body {
     font-family: 'Roboto', sans-serif;
     color: ${props => props.theme.black};
     background-color: ${props => props.theme.background};
   }
+
   a {
     color: ${props => props.theme.primary};
     font-weight: bold;
     text-decoration: none;
     transition: 0.3s;
   }
+
   a:hover {
     color: ${props => props.theme.primaryHover};
   }
 `
+
 function App ({ Component, pageProps }) {
+  useEffect(() => {
+    moment.locale('pt-br')
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
@@ -28,4 +41,5 @@ function App ({ Component, pageProps }) {
     </ThemeProvider>
   )
 }
+
 export default App
